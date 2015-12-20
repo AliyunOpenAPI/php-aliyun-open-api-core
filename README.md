9c5ed067f671c2f7caf41f6c3da338d0da42702e
lokielse <lokielse@gmail.com>
2015-12-20 20:15:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+The Core SDK for Aliyun OpenAPI
+===============================
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Total Downloads][ico-downloads]][link-downloads]
+
+The Core SDK for Aliyun OpenApi
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require lokielse/aliyun-open-api-core
+```
+
+## Related
+
+``` bash
+$ composer require lokielse/aliyun-open-api-core
+$ composer require lokielse/aliyun-open-api-ecs
+$ composer require lokielse/aliyun-open-api-mts
+$ composer require lokielse/aliyun-open-api-rds
+$ composer require lokielse/aliyun-open-api-slb
+```
---
+++ src/Regions/EndpointConfig.php
@@ -0,0 +1,84 @@
+<?php namespace Aliyun\Core\Regions;
+
+    /*
+     * Licensed to the Apache Software Foundation (ASF) under one
+     * or more contributor license agreements.  See the NOTICE file
+     * distributed with this work for additional information
+     * regarding copyright ownership.  The ASF licenses this file
+     * to you under the Apache License, Version 2.0 (the
+     * "License"); you may not use this file except in compliance
+     * with the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing,
+     * software distributed under the License is distributed on an
+     * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+     * KIND, either express or implied.  See the License for the
+     * specific language governing permissions and limitations
+     * under the License.
+     */
+
+/**
+ * Class EndpointConfig
+ * @package Aliyun\Core\Regions
+ * @usage
+ * $endpoint       = new Endpoint("cn-hangzhou", $regionIds, $productDomains);
+ * $endpoints      = array( $endpoint );
+ * EndpointProvider::setEndpoints($endpoints);
+ */
+
+define('ENABLE_HTTP_PROXY', false);
+define('HTTP_PROXY_IP', '127.0.0.1');
+define('HTTP_PROXY_PORT', '8888');
+
+class EndpointConfig
+{
+
+    private static $regionIds = array(
+        "cn-hangzhou",
+        "cn-beijing",
+        "cn-qingdao",
+        "cn-hongkong",
+        "cn-shanghai",
+        "us-west-1",
+        "cn-shenzhen",
+        "ap-southeast-1"
+    );
+
+
+    public static function getRegionIds()
+    {
+        return self::$regionIds;
+    }
+
+
+    public static function getProductDomains()
+    {
+        return array(
+            new ProductDomain("Ecs", "ecs.aliyuncs.com"),
+            new ProductDomain("Rds", "rds.aliyuncs.com"),
+            new ProductDomain("Mts", "mts.aliyuncs.com"),
+            new ProductDomain("BatchCompute", "batchCompute.aliyuncs.com"),
+            new ProductDomain("Bss", "bss.aliyuncs.com"),
+            new ProductDomain("Oms", "oms.aliyuncs.com"),
+            new ProductDomain("Slb", "slb.aliyuncs.com"),
+            new ProductDomain("Oss", "oss-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("OssAdmin", "oss-admin.aliyuncs.com"),
+            new ProductDomain("Sts", "sts.aliyuncs.com"),
+            new ProductDomain("Yundun", "yundun-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Risk", "risk-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Drds", "drds.aliyuncs.com"),
+            new ProductDomain("M-kvstore", "m-kvstore.aliyuncs.com"),
+            new ProductDomain("Ram", "ram.aliyuncs.com"),
+            new ProductDomain("Cms", "metrics.aliyuncs.com"),
+            new ProductDomain("Crm", "crm-cn-hangzhou.aliyuncs.com"),
+            new ProductDomain("Ocs", "pop-ocs.aliyuncs.com"),
+            new ProductDomain("Ots", "ots-pop.aliyuncs.com"),
+            new ProductDomain("Dqs", "dqs.aliyuncs.com"),
+            new ProductDomain("Location", "location.aliyuncs.com"),
+            new ProductDomain("Ubsms", "ubsms.aliyuncs.com"),
+            new ProductDomain("Ubsms-inner", "ubsms-inner.aliyuncs.com")
+        );
+    }
+}
